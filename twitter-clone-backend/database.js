267888322497f7c490d744ec92f475a90a7c79c5
Rajinikanth <rajnikanthgatti97@gmail.com>
2021-11-09 18:57:52 +0530

    no messafe

--- twitter-clone-backend/database.js
+++ twitter-clone-backend/database.js
@@ -5,7 +5,7 @@ class Database {
         this.connect();
     }
     connect() {
-        mongoose.connect("mongodb+srv://rajinikanth:test123@twitterclonecluster.ycsus.mongodb.net/TwitterCloneDB?retryWrites=true&w=majority")
+        mongoose.connect("mongodb+srv://<UserName>:<Password>@twitterclonecluster.ycsus.mongodb.net/TwitterCloneDB?retryWrites=true&w=majority")
         .then(()=>{
             console.log("Successfully connected to DB")
         }).catch(()=>{
